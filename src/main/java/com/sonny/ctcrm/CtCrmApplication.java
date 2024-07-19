047c1ad3564b386eb75434e1ef241b596357a83f
SonnyHardy <htambeck@gmail.com>
2024-07-19 20:40:48 +0200

    Projet inital

---
+++ src/main/java/com/sonny/ctcrm/CtCrmApplication.java
@@ -0,0 +1,13 @@
+package com.sonny.ctcrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtCrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CtCrmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sonny/ctcrm/CtCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sonny.ctcrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CtCrmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
