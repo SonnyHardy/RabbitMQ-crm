29bb8caea37575b3457b9d30559831e2355ec138
SonnyHardy <htambeck@gmail.com>
2024-07-19 21:43:43 +0200

    Lecture du message recu dans les queues

---
+++ src/main/java/com/sonny/ctcrm/Compte.java
@@ -0,0 +1,9 @@
+package com.sonny.ctcrm;
+
+public record Compte(
+        String prenom,
+        String nom,
+        String email,
+        String job
+) {
+}
---
+++ src/main/java/com/sonny/ctcrm/RabbitMQMessageHandler.java
@@ -0,0 +1,15 @@
+package com.sonny.ctcrm;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class RabbitMQMessageHandler {
+
+    @RabbitListener(queues = "#{${crm.queues}.split(',')}")
+    public void lireMessage(Compte message){
+        log.info("Nouveau message {}", message);
+    }
+}
